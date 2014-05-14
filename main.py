9ad729481ac91fd5f5bd39888e914e53441e7534
KevinBowdler <KevinBowdler@users.noreply.github.com>
2014-05-14 11:49:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import webapp2
 from google.appengine.ext import ndb
 from random import randrange
-
 quotes = ['Remember computers laugh at us when we do things manually, script everything.','All computers wait at the same speed.','Willyoupleasehelpmefixmykeyboard?Thespacebarisbroken!','A printer consists of three main parts: the case, the jammed paper tray and the blinking red light.']
-
 
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
@@ -11,9 +9,3 @@ class MainHandler(webapp2.RequestHandler):
 		self.response.out.write(quotes[randrange(3)])
 
 app = webapp2.WSGIApplication([('/', MainHandler),],debug=True)
-
-def main():
-app.run()
-
-if __name__ == '__main__':
-main()
