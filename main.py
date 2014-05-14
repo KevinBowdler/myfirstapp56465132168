0a8e22c50635fe135a12c08d51e64ed7d93a968f
KevinBowdler <KevinBowdler@users.noreply.github.com>
2014-05-14 11:37:06 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import webapp2
+from google.appengine.ext import ndb
+from random import randrange
+
+quotes = ['Remember computers laugh at us when we do things manually, script everything.','All computers wait at the same speed.','Willyoupleasehelpmefixmykeyboard?Thespacebarisbroken!','A printer consists of three main parts: the case, the jammed paper tray and the blinking red light.']
+
+
+class MainHandler(webapp2.RequestHandler):
+	def get(self):
+		self.response.headers["Content-Type"] = "text/plain"
+		self.response.out.write(quotes[randrange(3)])
+
+app = webapp2.WSGIApplication([('/', MainHandler),],debug=True)
+
+def main():
+app.run()
+
+if __name__ == '__main__':
+main()
